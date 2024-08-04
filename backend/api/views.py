0e4f9b16a0542c5dd7c17bb1b3f2c79746d0d667
illustrious-codes <onatayodavid105@gmail.com>
2024-08-04 22:53:28 +0100

    backend

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,15 @@
-from urllib import request
-from django.http import JsonResponse
+
+from pyexpat import model
+from django.http import HttpResponse, JsonResponse
+import json
+from django.forms.models import model_to_dict
+
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    body = request.body
-    print(body)
-    return JsonResponse({"David": "Hi dear, this is your first api response"})
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=["id", "price"])
+    return HttpResponse(data)
+    # return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,9 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_request = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"})
-
-# print(get_request.text)
+get_request = requests.get(endpoint, json={"query": "Hello World"})
+print(get_request.headers)
+print(get_request.text)
 # print(get_request.status_code)
-print(get_request.json())
+# print(get_request.text)
+# print(get_request.json())
